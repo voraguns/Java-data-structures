15c892f1073a93b3f11c1cd064e6d7c72dae91b0
Voragun Supawong <voragun2543@gmail.com>
2022-09-13 16:37:13 +0700

    [new style] Example chain

--- code/Start.java
+++ code/Start.java
@@ -1,33 +1,32 @@
 class Start {
     public static void main(String[] data) {
-        Element e; // no default value
-        Element f; // no default value
-        Element g; // no default value
-        
-        e = new Element();
-        e.name = "Tom";
-        e.numnber = 1;
-        f = new Element();
-        f.name = "Jerry";
-        f.numnber = 2;
-        g = new Element();
-        g.name = "Spike";
-        g.numnber = 3;
-        
-        e.next = f; // link first and second
-        f.next = g; // link second and third
-        
-        for (Element current = e; current != null; current = current.next) {
-            System.out.println(current.numnber + " " + current.name);
-        }
+       Block a = new Block();
+       a.name = "Gun";
+       a.number = 1;
+       
+       Block b = new Block();
+       b.name = "Tom";
+       b.number = 2;
+       
+       Block c = new Block();
+       c.name = "Jerry";
+       c.number = 3;
+       
+       // Write code fragment to print all element from the latest element
+       c.back = b;
+       b.back = a;
+       
+       for (Block current = c; current != null; current = current.back) {
+           System.out.println(current.number + " " + current.name);
+       }
+       
     }
     
     
 }
 
-class Element {
-    String name; // null
-    int numnber; // 0
-    double weight; // 0.0
-    Element next; // null
+class Block {
+    Block back;    // address of the previous block (element)
+    String name;
+    int number;
 }
